f447ffba06bb178a2b3e093b6e408dac8f9f3e38
jaehyeok34 <hyeok9942@gmail.com>
2024-10-12 10:48:17 +0000

    1012 task

--- include/simple_decorator2.hpp
+++ include/simple_decorator2.hpp
@@ -1,14 +1,14 @@
 #pragma once
 #include "base_decorator.hpp"
 
-class SimpleDecorator: public BaseDecorator
+class SimpleDecorator2: public BaseDecorator
 {
 public:
-    SimpleDecorator(CAgent* agent):BaseDecorator(agent){}; // 생성자: 객체 생성하는 애
+    SimpleDecorator2(CAgent* agent):BaseDecorator(agent){}; // 생성자: 객체 생성하는 애
     
 public:
     virtual bool detect(CAgent* pTarget) {                  // 재정의된 메소드: 
-        std::cout << "Simple Decorator" << std::endl;
+        std::cout << "Simple Decorator2" << std::endl;
         return m_wrappee->detect(pTarget);
     };
 };
--- src/app.cpp
+++ src/app.cpp
@@ -1,42 +1,120 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
 #include "agent.hpp"
 #include "mmanager.hpp"
 #include "stochastic_decorator.hpp"
 #include "simple_decorator.hpp"
+#include "tinyxml2.h"
+#include "special_agent.hpp"
+#include "simple_decorator2.hpp"
 
-int main(int argc, char** argv)
-{// Clinet Code  
+class InitManager {
+public:
+    InitManager() { }
+    InitManager(std::string path) {
+        // tinyxml2의 XMLDocument 인스턴스를 통해 
+        // InitManager의 생성자 매개변수 path에 해당하는 xml을 연다
+        doc.LoadFile(path.c_str()); 
+        double x, y, heading, speed, drange;
+        
+        // CAgent 생성 과정
+        {
+            // XML 파일에서 "scenario"에 해당하는 첫번째 자식 요소 밑에 있는 "AgentList" 자식 요소를 가져오고 있음
+            tinyxml2::XMLElement* agListElem = doc.FirstChildElement("scenario")
+                                                    ->FirstChildElement("AgentList");      
+            
+            for(
+                tinyxml2::XMLElement* agElem = agListElem->FirstChildElement();
+                agElem != NULL;
+                agElem = agElem->NextSiblingElement()
+            ) {
+                // XML 파일로 부터 값을 가져와서 변수에 저장
+                agElem->QueryDoubleAttribute("x", &x);
+                agElem->QueryDoubleAttribute("y", &y);
+                agElem->QueryDoubleAttribute("heading", &heading);
+                agElem->QueryDoubleAttribute("speed", &speed);
+                agElem->QueryDoubleAttribute("drange", &drange);
+
+                // 저장된 값을 기반으로 CAgent instacne 생성
+                CAgent* ag = new CAgent(x, y, heading, speed, drange);
+
+                // #01 과제와 동일한 출력을 위해 추가
+                ag = new SimpleDecorator(new SimpleDecorator2(new StochasticDecorator(ag)));
+
+                // 생성된 agent 객체를 리스트에 추가
+                m_agentList.push_back(ag);
+            }
+        }
+
+        // CSpecialAgent 생성 과정
+        {
+            tinyxml2::XMLElement* agListElem = doc.FirstChildElement("scenario")
+                                                    ->FirstChildElement("SAgentList");      
+            for(
+                tinyxml2::XMLElement* agElem = agListElem->FirstChildElement();
+                agElem != NULL; 
+                agElem = agElem->NextSiblingElement()
+            ) {
+                agElem->QueryDoubleAttribute("x", &x);
+                agElem->QueryDoubleAttribute("y", &y);
+                agElem->QueryDoubleAttribute("heading", &heading);
+                agElem->QueryDoubleAttribute("speed", &speed);
+                agElem->QueryDoubleAttribute("drange", &drange);
+
+                // CAgent instance가 아닌 CSpecialAgent instance를 생성하고 있음
+                CAgent* ag = new CSpecialAgent(x, y, heading, speed, drange);
+
+                // #01 과제와 동일한 출력을 위해 추가
+                ag = new SimpleDecorator(new SimpleDecorator2(new StochasticDecorator(ag)));
+
+                m_agentList.push_back(ag);
+            }
+        }
+    }
+
+    std::vector<CAgent*>& get_agent_list() {
+        return m_agentList;
+    }    
+
+private:
+    tinyxml2::XMLDocument doc;
+    std::vector<CAgent*> m_agentList;
+};
+
+int main(int argc, char** argv) {
+
+    // XML 파일 path를 전달하여 tinyxml2를 통해 agent의 값을 가져와 agent 생성
+    InitManager init_manager("test.xml");
 
     /* Simulation Engine */
     CManeuverManager mmanager;
 
-    /* Simulation Model Registration */
-    CAgent* p_agent1 = new CAgent(10,  0, 0, 1, 10);
-    p_agent1 = new StochasticDecorator(p_agent1);
-    p_agent1 = new SimpleDecorator(p_agent1);
-    
-    mmanager.register_publisher(p_agent1);
+    for (
+        std::vector<CAgent*>::iterator iter = init_manager.get_agent_list().begin();
+        iter != init_manager.get_agent_list().end(); 
+        ++iter 
+    ) {
+        mmanager.register_publisher(*iter); 
+    }
 
-    CAgent* p_agent2 = new CAgent(10,  30, 180, 1, 10);
-    mmanager.register_publisher(p_agent2);
-    
     /* Simulation Engine Initialization */
     double sim_time;
     double time_step = 1;
 
-    for(sim_time = 0; sim_time < 30; sim_time += time_step)
-    {   
-        //p_agent1->maneuver(time_step);
-        //p_agent2->maneuver(time_step);
-
-        //p_agent1->detect(p_agent2);
-        //p_agent2->detect(p_agent1);
-
+    for (sim_time = 0; sim_time < 30; sim_time += time_step) {   
         mmanager.svc(time_step);
 
         std::cout << "----" << std::endl;
-        std::cout << "Time: " << sim_time << ", " <<*p_agent1 << std::endl;
-        std::cout << "Time: " << sim_time << ", " <<*p_agent2 << std::endl;
+        for(
+            std::vector<CAgent*>::iterator iter = init_manager.get_agent_list().begin();
+            iter != init_manager.get_agent_list().end(); 
+            ++iter 
+        ) {
+            std::cout << "Time: " << sim_time << ", " << **iter << std::endl;
+        }
     }
+    
     return 0;
 }
